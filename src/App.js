de6efce2b424ea9f661941b7499253be92e08741
Adam Friedl <adam.friedl@rakuten.com>
2019-07-10 14:55:10 -0400

    load fetched data and add spinner

--- src/App.js
+++ src/App.js
@@ -5,16 +5,33 @@ import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+    state = {
+        users: [],
+        loading: false,
+    };
+
     async componentDidMount() {
-        const res = await axios.get('https://api.github.com/users');
-        console.log(res.data);
+        this.setState({ loading: true });
+        const res = await axios.get(
+            `https://api.github.com/users?client_id=${
+                process.env.REACT_APP_GITHUB_CLIENT_ID
+            }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        );
+        this.setState({
+            users: res.data,
+            loading: false,
+        });
     }
+
     render() {
         return (
             <div className='App'>
                 <Navbar />
                 <div className='container'>
-                    <Users />
+                    <Users
+                        loading={this.state.loading}
+                        users={this.state.users}
+                    />
                 </div>
             </div>
         );
